2c28f3948fc499078341b82f5e4386ff4dcda247
M4nticore1 <144529539+M4nticore1@users.noreply.github.com>
2026-03-03 15:36:35 +0300

    new: Google Play In-App Update

---
+++ Assets/Scripts/GooglePlay/GooglePlayInAppUpdate.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using Google.Play.AppUpdate;
+using Google.Play.Common;
+using TMPro;
+using System.Collections;
+
+public class GooglePlayInAppUpdate : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI inAppStatus;
+    private AppUpdateManager appUpdateManager;
+
+    private void Start()
+    {
+        appUpdateManager = new AppUpdateManager();
+        //StartCoroutine(CheckForUpdate());
+    }
+
+    private IEnumerator CheckForUpdate()
+    {
+        PlayAsyncOperation<AppUpdateInfo, AppUpdateErrorCode> appUpdateInfoOperation = appUpdateManager.GetAppUpdateInfo();
+
+        yield return appUpdateInfoOperation;
+
+        var appUpdateInforResult = appUpdateInfoOperation.GetResult();
+
+        if (appUpdateInfoOperation.IsSuccessful) {
+            if (appUpdateInforResult.UpdateAvailability == UpdateAvailability.UpdateAvailable) {
+                inAppStatus.SetText(UpdateAvailability.UpdateAvailable.ToString());
+            }
+            else {
+                inAppStatus.SetText("No Update Avaliable");
+            }
+        }
+
+        var appUpdateOptions = AppUpdateOptions.ImmediateAppUpdateOptions();
+
+        StartCoroutine(StartImmediateUpdate(appUpdateInforResult, appUpdateOptions));
+    }
+
+    private IEnumerator StartImmediateUpdate(AppUpdateInfo appUpdateInfo, AppUpdateOptions appUpdateOptions)
+    {
+        var startUpdateRequest = appUpdateManager.StartUpdate(appUpdateInfo, appUpdateOptions);
+
+        yield return startUpdateRequest;
+    }
+}
